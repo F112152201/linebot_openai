a242d45c81d9ae2cbb48f89d0a03a67221dde705
F112152201 <156160196+F112152201@users.noreply.github.com>
2024-03-20 00:58:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
-
-from flask import request, abort
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 import os
 
+app = Flask(__name__)
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 @app.route('/callback', methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     try:
-        handler1.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text1 = event.message.text
     user_profile = {
-        "occupation": "professor",  # 將用戶的職業設定為 "教授"，可以根據需要修改這個值
-        "ability": "Research"   # 將用戶的能力設定為 "研究"，可以根據需要修改這個值
+        "occupation": "professor",
+        "ability": "Research"
     }
     response = openai.ChatCompletion.create(
         messages=[
             {"role": "user", "content": text1},
-            {"role": "system", "content": user_profile}  # 將用戶資料添加到請求中
+            {"role": "system", "content": user_profile}
         ],
         model="gpt-3.5-turbo-0125",
         temperature=0.5,
