44fe469b50709b5598b15e956832eced8cb0d1bf
F112152201 <156160196+F112152201@users.noreply.github.com>
2024-03-19 13:13:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 import os
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler1 = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route('/callback', methods=['POST'])
 def callback():
@@ -25,18 +22,22 @@ def callback():
 @handler1.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text1=event.message.text
+    user_profile = {
+        "occupation": "professor",  # 將用戶的職業設定為 "分析師"，可以根據需要修改這個值
+        "ability": "Research and Design"   # 將用戶的能力設定為 "分析股票"，可以根據需要修改這個值
     response = openai.ChatCompletion.create(
         messages=[
             {"role": "user", "content": text1}
+            {"role": "system", "content": user_profile}  # 將用戶資料添加到請求中
         ],
         model="gpt-3.5-turbo-0125",
         temperature = 0.5,
-    )
     try:
         ret = response['choices'][0]['message']['content'].strip()
     except:
         ret = '發生錯誤！'
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=ret))
 
 if __name__ == '__main__':
     app.run()
+
